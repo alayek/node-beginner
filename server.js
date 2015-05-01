a847ecf68db4e7b13ffac003eaf997770bd07bcd
Arijit Layek <arijit.layek1991@gmail.com>
2015-05-01 23:59:57 +0530

    Modularize further. Keep server in separate file

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require("http");
 
-function onRequest (request, response) {
-	// body...
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	response.write("Hello World!");
-	response.end();
+function start () {
+	// request handler
+	function onRequest (request, response) {
+
+		response.writeHead(200, {'Content-Type':'text/plain'});
+		response.write("Hello World!");
+		response.end();
+	}
+
+	// start the server with the callback
+	http.createServer(onRequest).listen(8888);
+
+	console.log("** Server Started on port 8888 **");
 }
 
-http.createServer(onRequest).listen(8888);
 
-console.log("** Server Started on port 8888**");
+
+exports.start = start;
