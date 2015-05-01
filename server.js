1fa87ec7a07a9644ca2dd73f4f375385ddf5b68f
Arijit Layek <arijit.layek1991@gmail.com>
2015-05-02 00:12:20 +0530

    Seperate between request urls

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require("http");
+var url = require("url");
 
 function start (portProvided) {
 	// request handler
 	function onRequest (request, response) {
-
+		var pathname = url.parse(request.url).pathname;
+		console.log('request for ' + pathname + ' recieved.')
 		response.writeHead(200, {'Content-Type':'text/plain'});
 		response.write("Hello World!");
 		response.end();
